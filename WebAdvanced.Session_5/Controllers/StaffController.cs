4c7c752b8c448edbb09050d328078a134a795585
Binhkha <hobinhkha22@gmail.com>
2017-03-05 16:38:23 +0700

    Update Session 5 (Tuan 5)

---
+++ WebAdvanced.Session_5/Controllers/StaffController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAdvanced.Session_5.Models;
+
+namespace WebAdvanced.Session_5.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(FormCollection form)
+        {
+            // lay thong tin tu input
+            var _fhinh = Request.Files["MyFileImage"];
+            // save hinh ve server -> successful
+            var _pathhinh = Server.MapPath("~/Images/" + _fhinh.FileName);
+            _fhinh.SaveAs(_pathhinh);
+            string _path = Server.MapPath("~/Staffinfo.txt");
+            string[] info = { form["txtID"], form["txtName"], form["txtDate"], form["txtSalary"], _fhinh.FileName };
+            // ghi
+            System.IO.File.WriteAllLines(_path, info);
+            return View("Index");
+        }
+
+        public ActionResult Open()
+        {
+            // doc lai
+            string _path = Server.MapPath("~/Staffinfo.txt");
+            string[] info = System.IO.File.ReadAllLines(_path);
+            Staff st = new Staff();
+            st._staffID = int.Parse(info[0]);
+            st._staffName = info[1];
+            st._birthofDate = DateTime.Parse(info[2]);
+            st._salary = decimal.Parse(info[3]);
+            st._staffImage = info[4];
+
+            // chuyen
+            ViewBag.id = st._staffID;
+            ViewBag.name = st._staffName;
+            ViewBag.birthday = st._birthofDate.ToString("yyyy-MM-dd");
+            ViewBag.salary = st._salary;
+            ViewBag.staffimage = "../../Images/" + st._staffImage;
+                
+            return View("Index");
+        }
+    }
+}
---
+++ WebAdvanced.Session_5/Models/Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAdvanced.Session_5.Models
+{
+    public class Staff
+    {
+        public int _staffID { get; set; }
+        public string _staffName { get; set; }
+        public DateTime _birthofDate { get; set; }
+        public decimal _salary { get; set; }
+        public string _staffImage { get; set; }
+    }
+}
